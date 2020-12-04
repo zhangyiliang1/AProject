3c31fa6c385c59cdc51ab59617661b11f486dd1f
zhangyiliang_v <zhangyiliang_v@hongxin>
2020-12-04 15:19:06 +0800

    头部

--- src/page/index.vue
+++ src/page/index.vue
@@ -1,44 +1,85 @@
+<style scoped>
+.head {
+  width: 100%;
+  background: #348FF0;
+}
 
+.nav-wrap{
+  display: flex;
+  justify-content: space-around;
+  line-height: 80px;
+  color: #fff;
+  
+}
+.nav-wrap div:hover{
+   cursor: pointer;
+   color: #ffae00;
+}
+.logo{
+    height: 60px;
+    width: 100px;
+    padding: 10px 0;
+    margin: 0px auto;
+    border-radius: 50%;
+}
+.logo img{
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+}
+.head-left {
+  text-align: center;
+  flex: 2;
+
+}
+.head-center {
+  flex: 7;
+}
+.head-right {
+  flex: 3;
+}
+.search{
+  width: 300px;
+  height: 40px;
+  line-height: 40px;
+  font-size: 18px;
+  margin: 20px 0;
+  border: 1px solid #f5f5f5;
+  border-radius: 100px;
+  padding-left: 20px;
+  color: #fff;
+}
+.search img{
+  width: 20px;
+  height: 20px;
+  vertical-align:middle;
+}
+
+</style>
 <template>
   <div>
     <div>
       <div class="head dis">
         <div class="head-left">
-          <img src="@/assets/banner.png" alt="">
+        <div class="logo">
+ <img src="@/assets/banner.png" alt="" />
+        </div>
+         
         </div>
         <div class="head-center">
-          <el-menu
-          :default-active="activeIndex2"
-          class="el-menu-demo"
-          mode="horizontal"
-          @select="handleSelect"
-          text-color="#333"
-          text-size='30px'
-          active-text-color="#ffd04b"
-        >
-          <el-menu-item index="1">处理中心</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="3" disabled>消息中心</el-menu-item>
-          <el-menu-item index="4"
-            ><a href="https://www.ele.me" target="_blank"
-              >订单管理</a
-            ></el-menu-item
-          >
-        </el-menu>
+          <div class="nav-wrap">
+            <div>首页</div>
+            <div>购物中心</div>
+            <div>生活服务</div>
+            <div>我的购物车</div>
+          </div>
         </div>
-        <div class="head-right">搜索</div>
-        
+        <div class="head-right">
+           <div class="search">
+           <img src="@/assets/icon_search.png" alt="">
+             请输入你想搜索的商品
+           </div>
+        </div>
       </div>
     </div>
   </div>
@@ -48,38 +89,15 @@
 <script>
 export default {
   data() {
-      return {
-        activeIndex: '1',
-        activeIndex2: '1'
-      };
+    return {
+      activeIndex: "1",
+      activeIndex2: "1",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
     },
-    methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-      }
-    }
+  },
 };
 </script>
-<style  scoped>
-.head {
-  width: 100%;
- 
-  height: 60px;
-}
-.head-left img{
-  width: 100%;
-  height: 100%;
-}
-.head-left{
-  flex: 1;
-   border: 1px solid red;
-  }
-.head-center{
-  flex: 7;
-   border: 1px solid red;
-}
-.head-right{
-  flex: 4;
-   border: 1px solid red;
-}
-</style>
